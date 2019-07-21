9978c1f8d59e8c3bfac69a010bc3cc1f3a726ea5
Roy Ivy III <rivy.dev@gmail.com>
2019-07-21 10:59:38 -0500

    change ~ improve/update example for new API

--- eg/show-paths.js
+++ eg/show-paths.js
@@ -2,6 +2,57 @@
 /* eslint-env es6, node */
 'use strict';
 
-const p = require('..')('test');
+const path = require('path');
+const util = require('util');
+const _ = require('lodash') || undefined;
 
-console.log(p);
+const appPaths = require('..');
+
+// Extend appPaths with a "log" location
+appPaths.log = () => {
+	return path.join(appPaths.state(), 'log');
+};
+
+console.log('appPaths:', util.inspect(appPaths));
+if (_) {
+	_.each(appPaths, (value, key) => {
+		console.log(key, '=', appPaths[key]());
+	});
+}
+
+delete process.env.XDG_CONFIG_HOME;
+let p = require('..')('dross');
+
+console.log('p:', util.inspect(p));
+if (_) {
+	_.each(p, (value, key) => {
+		console.log(key, '=', p[key]());
+	});
+}
+
+p = require('..')({suffix: '-nodejs'});
+
+console.log('p:', util.inspect(p));
+if (_) {
+	_.each(p, (value, key) => {
+		console.log(key, '=', p[key]());
+	});
+}
+
+p = require('..')({name: 'extraordinaire', suffix: '-nodejs'});
+
+console.log('p:', util.inspect(p));
+if (_) {
+	_.each(p, (value, key) => {
+		console.log(key, '=', p[key]());
+	});
+}
+
+p = require('..')({name: 'fluffy', isolated: false});
+
+console.log('p:', util.inspect(p));
+if (_) {
+	_.each(p, (value, key) => {
+		console.log(key, '=', p[key]());
+	});
+}
