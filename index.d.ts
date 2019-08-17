7a99d84f6667133b455437bafbed8a9298d4b35c
Roy Ivy III <rivy.dev@gmail.com>
2019-08-17 17:43:17 -0500

    fix typescript definitions and testing

--- index.d.ts
+++ index.d.ts
@@ -1,66 +1,16 @@
 // # spell-checker:ignore rivy
-declare namespace xdgAppPaths {
-	export interface Paths {
-		/**
-		Directory for non-essential data files.
-		*/
-		cache(): string;
+declare namespace XDGAppPaths {
+	function cache(dir_options?: boolean | {isolated: boolean}): string;
+	function config(dir_options?: boolean | {isolated: boolean}): string;
+	function data(dir_options?: boolean | {isolated: boolean}): string;
+	function runtime(dir_options?: boolean | {isolated: boolean}): string | undefined;
+	function state(dir_options?: boolean | {isolated: boolean}): string;
 
-		/**
-		Directory for data files.
-		*/
-		config(): string;
+	function configDirs(dir_options?: boolean | {isolated: boolean}): string[];
+	function dataDirs(dir_options?: boolean | {isolated: boolean}): string[];
 
-		/**
-		Directory for data files.
-		*/
-		data(): string;
-
-		runtime(): string | undefined;
-
-		/**
-		Directory for state files.
-		*/
-		state(): string;
-
-		/**
-		Directory for temporary files.
-		*/
-		temp(): string;
-
-		configDirs(): string[];
-		dataDirs(): string[];
-	}
+	function $name(): string;
+	function $isolated(): boolean;
 }
-
-declare const xdgAppPaths: {
-	/**
-	Get OS-specific (and [XDG](https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html)-compatible) paths for storing things like data, config, cache, etc
-
-	Note: It only generates the path strings. It doesn't create the directories for you. You could use [`make-dir`](https://github.com/sindresorhus/make-dir) to create the directories.
-
-	@param name - Name of your project. Used to generate the paths.
-	@returns The paths to use for your project on current OS.
-
-	@example
-	```
-	import xdgAppPaths = require('xdg-app-paths');
-
-	const paths = xdgAppPaths('MyApp');
-
-	paths.data;
-	//(*nix)=> '/home/rivy/.local/share/MyApp-nodejs'
-
-	paths.config
-	//(*nix)=> '/home/rivy/.config/MyApp-nodejs'
-	```
-	*/
-	(name: string): xdgAppPaths.Paths;
-
-	// TODO: Remove this for the next major release, refactor the whole definition to:
-	// declare function xdgAppPaths(name: string, options?: xdgAppPaths.Options): xdgAppPaths.Paths;
-	// export = xdgAppPaths;
-	default: typeof xdgAppPaths;
-};
-
-export = xdgAppPaths;
+declare function XDGAppPaths( options?: string | {name: string, suffix: string, isolated: boolean} ): typeof XDGAppPaths;
+export = XDGAppPaths;
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,14 +1,33 @@
 import {expectType} from 'tsd';
 import xdgAppPaths = require('.');
-import {Paths} from '.';
 
-expectType<Paths>(xdgAppPaths('MyApp'));
+const paths: typeof xdgAppPaths = xdgAppPaths('MyApp');
 
-const paths = xdgAppPaths('MyApp');
+expectType<typeof xdgAppPaths>(xdgAppPaths());
+expectType<typeof xdgAppPaths>(xdgAppPaths('MyApp'));
+expectType<typeof xdgAppPaths>(xdgAppPaths({name: 'MyApp', suffix: '-nodejs', isolated: false}));
 
 expectType<string>(paths.cache());
+expectType<string>(paths.cache(false));
+expectType<string>(paths.cache({isolated: true}));
 expectType<string>(paths.config());
+expectType<string>(paths.config(true));
+expectType<string>(paths.config({isolated:false}));
 expectType<string>(paths.data());
+expectType<string>(paths.data(false));
+expectType<string>(paths.data({isolated: true}));
 expectType<string|undefined>(paths.runtime());
+expectType<string|undefined>(paths.runtime(true));
+expectType<string|undefined>(paths.runtime({isolated: false}));
 expectType<string>(paths.state());
-expectType<string>(paths.temp());
+expectType<string>(paths.state(false));
+expectType<string>(paths.state({isolated: true}));
+expectType<string[]>(paths.configDirs());
+expectType<string[]>(paths.configDirs(true));
+expectType<string[]>(paths.configDirs({isolated: false}));
+expectType<string[]>(paths.dataDirs());
+expectType<string[]>(paths.dataDirs(false));
+expectType<string[]>(paths.dataDirs({isolated: true}));
+
+expectType<string>(paths.$name());
+expectType<boolean>(paths.$isolated());
