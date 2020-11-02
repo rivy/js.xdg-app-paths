3d0a586802bc4852cfbc1fdafacc2e6195c810a8
Roy Ivy III <rivy.dev@gmail.com>
2020-11-01 23:42:09 -0600

    refactor ~ minimize use of `typeof x` to avoid loss of linter variable tracking
    ## [why]
    
    The variable `x` in `typeof x` is not tracked for prior declaration/definition by some
    linters. So, minor typos can lead to hidden "wrong variable use" bugs. For example, using
    `typeof xx` instead of the desired `typeof x` where `xx` is never defined, is a bug which
    eslint will not detect, even when `xx` is never defined.
    
    Using the replacement `typeOf(x)` does enables checking for prior declaration/definition
    of `x` and will detect inadvertent unknown variable bugs.

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,18 +7,34 @@ const path = require('path');
 const xdg = require('xdg-portable');
 
 function normalizeOptions_(options, isolated) {
-	if (typeof options !== 'object') {
+	if (!isObject(options)) {
 		options = {isolated: options};
 	}
 
 	options = options || {};
 
 	options.isolated = ((options.isolated === undefined) || (options.isolated === null)) ? isolated : options.isolated;
-	if (typeof options.isolated !== 'boolean') {
-		throw new TypeError(`Expected boolean for "isolated" argument, got ${typeof options.isolated}`);
+	if (!isBoolean(options.isolated)) {
+		throw new TypeError(`Expected boolean for "isolated" argument, got ${typeOf(options.isolated)}`);
 	}
 
 	return options;
+}
+
+function isBoolean(value) {
+	return typeOf(value) === 'boolean';
+}
+
+function isObject(value) {
+	return typeOf(value) === 'object';
+}
+
+function isString(value) {
+	return typeOf(value) === 'string';
+}
+
+function typeOf(value) {
+	return typeof value;
 }
 
 class XDGAppPaths_ {
@@ -27,7 +43,7 @@ class XDGAppPaths_ {
 			return new XDGAppPaths_(options).fn;
 		};
 
-		if (typeof options !== 'object') {
+		if (!isObject(options)) {
 			options = {name: options};
 		}
 
@@ -35,20 +51,20 @@ class XDGAppPaths_ {
 
 		options.isolated = ((options.isolated === undefined) || (options.isolated === null)) ? true : options.isolated;
 		const isolated_ = options.isolated;
-		if (typeof isolated_ !== 'boolean') {
-			throw new TypeError(`Expected boolean for "isolated" argument, got ${typeof isolated_}`);
+		if (!isBoolean(isolated_)) {
+			throw new TypeError(`Expected boolean for "isolated" argument, got ${typeOf(isolated_)}`);
 		}
 
 		options.suffix = ((options.suffix === undefined) || (options.suffix === null)) ? '' : options.suffix;
 		const suffix_ = options.suffix;
-		if (typeof suffix_ !== 'string') {
-			throw new TypeError(`Expected string for "suffix" argument, got ${typeof suffix_}`);
+		if (!isString(suffix_)) {
+			throw new TypeError(`Expected string for "suffix" argument, got ${typeOf(suffix_)}`);
 		}
 
 		options.name = ((options.name === undefined) || (options.name === null)) ? '' : options.name;
 		let name_ = options.name;
-		if (typeof name_ !== 'string') {
-			throw new TypeError(`Expected string for "name" argument, got ${typeof name_}`);
+		if (!isString(name_)) {
+			throw new TypeError(`Expected string for "name" argument, got ${typeOf(name_)}`);
 		}
 
 		// Derive a suitable application name (ref: <https://stackoverflow.com/a/46110961/43774>)
@@ -65,43 +81,43 @@ class XDGAppPaths_ {
 
 		XDGAppPaths.cache = (dirOptions = null) => {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return path.join(xdg.cache(), isolated ? name_ : '');
 		};
 
 		XDGAppPaths.config = function (dirOptions = null) {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return path.join(xdg.config(), isolated ? name_ : '');
 		};
 
 		XDGAppPaths.data = function (dirOptions = null) {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return path.join(xdg.data(), isolated ? name_ : '');
 		};
 
 		XDGAppPaths.runtime = function (dirOptions = null) {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return xdg.runtime() ? path.join(xdg.runtime() || '', isolated ? name_ : '') : undefined;
 		};
 
 		XDGAppPaths.state = function (dirOptions = null) {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return path.join(xdg.state(), isolated ? name_ : '');
 		};
 
 		XDGAppPaths.configDirs = function (dirOptions = null) {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return xdg.configDirs().map(s => path.join(s, isolated ? name_ : ''));
 		};
 
 		XDGAppPaths.dataDirs = function (dirOptions = null) {
 			dirOptions = normalizeOptions_(dirOptions, isolated_);
-			const isolated = typeof dirOptions === 'boolean' ? dirOptions : dirOptions.isolated;
+			const isolated = isBoolean(dirOptions) ? dirOptions : dirOptions.isolated;
 			return xdg.dataDirs().map(s => path.join(s, isolated ? name_ : ''));
 		};
 
