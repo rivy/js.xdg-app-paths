8a60f9890d5e48c45f9a81431ec8cfbfd3cbc9fb
Roy Ivy III <rivy.dev@gmail.com>
2021-02-14 20:52:30 -0600

    maint/dev ~ add RemarkLint config

---
+++ .remarkrc.js
@@ -0,0 +1,28 @@
+// spell-checker:ignore frontmatter retext
+exports.plugins = [
+	require('remark-footnotes'),
+	// require('remark-frontmatter'),
+	[
+		require('remark-retext'),
+		require('unified')().use({
+			plugins: [
+				require('retext-english'),
+				require('retext-syntax-urls'),
+				// [require('retext-spell'), require('dictionary-en')],
+				[require('retext-sentence-spacing'), { preferred: 1 }],
+				require('retext-repeated-words'),
+				require('retext-passive'),
+			],
+		}),
+	],
+	'remark-preset-lint-consistent',
+	'remark-preset-lint-recommended',
+	'remark-preset-lint-markdown-style-guide',
+	['remark-lint-emphasis-marker', 'consistent'],
+	['remark-lint-file-extension', false],
+	['remark-lint-heading-increment', false],
+	['remark-lint-list-item-indent', 'mixed'],
+	['remark-lint-maximum-line-length', false],
+	['remark-lint-no-duplicate-headings', false],
+	['remark-lint-unordered-list-marker-style', 'consistent'],
+];
