d41c81b2bc3fb85efcc90a801d6b096dd61c2c25
Roy Ivy III <rivy.dev@gmail.com>
2021-02-17 20:29:41 -0600

    tests ~ increase testing coverage of builds (via more fixtures)

---
+++ test/fixtures/cli-display-name.esm-wrapper.mjs
@@ -0,0 +1,7 @@
+/* eslint-env es6, node */
+/* eslint-disable no-console , security-node/detect-crlf */
+
+import p from '../../build/cjs/src/esm-wrapper/mod.esm.js';
+
+console.log(p.$name());
+// console.warn({ process });
---
+++ test/fixtures/cli-display-name.tests_.esm-wrapper.mjs
@@ -0,0 +1,6 @@
+/* eslint-env es6, node */
+/* eslint-disable no-console , security-node/detect-crlf */
+
+import p from '../../build/tests_/src/esm-wrapper/mod.esm.js';
+
+console.log(p.$name());
