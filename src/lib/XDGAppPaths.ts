9765b638731739cb864e4899d0b7c91ba97aed45
Roy Ivy III <rivy.dev@gmail.com>
2021-02-21 19:50:40 -0600

    docs ~ add JSDocs descriptions

--- src/lib/XDGAppPaths.ts
+++ src/lib/XDGAppPaths.ts
@@ -11,28 +11,54 @@ import { Platform } from '../platform-adapters/_base.js';
 // # ref: <https://wiki.debian.org/XDGBaseDirectorySpecification#state> @@ <http://archive.is/pahId>
 // # ref: <https://ploum.net/207-modify-your-application-to-use-xdg-folders> @@ <https://archive.is/f43Gk>
 
+/** Configuration options supplied to `XDGAppPaths` methods; (optional) */
 type DirOptions = {
 	readonly isolated?: boolean | null;
 };
 
+/** Configuration options supplied when constructing `XDGAppPaths`; (optional) */
 type Options = {
 	readonly name?: string | null;
 	readonly suffix?: string | null;
 	readonly isolated?: boolean | null;
 };
 
+/** `XDGAppPaths` (API) Determine (XDG-compatible) paths for storing application files (cache, config, data, etc) */
 type XDGAppPaths = {
+	/** Create an `XDG` object (`new` is optional). */
 	new (options?: Options | string): XDGAppPaths;
+	/** Create an `XDG` object (`new` is optional). */
 	(options?: Options | string): XDGAppPaths;
-	readonly $name: () => string;
-	readonly $isolated: () => boolean;
+
+	/** Returns the directory for non-essential data files.
+	> Deletion of the data contained here might cause an application to slow down.
+	*/
 	readonly cache: (dirOptions?: DirOptions | boolean) => string;
+	/** Returns the directory for config files.
+	> Deletion of the data contained here might require the user to reconfigure an application.
+	*/
 	readonly config: (dirOptions?: DirOptions | boolean) => string;
+	/** Returns the directory for data files.
+	> Deletion of the data contained here might force the user to restore from backups.
+	*/
 	readonly data: (dirOptions?: DirOptions | boolean) => string;
+	/** Returns the directory for runtime files; may return `undefined`.
+	> Deletion of the data contained here might interfere with a currently executing application but should have no effect on future executions.
+	*/
 	readonly runtime: (dirOptions?: DirOptions | boolean) => string | undefined;
+	/** Returns the directory for state files.
+	> Deletion of the data contained here should not materially interfere with execution of an application.
+	*/
 	readonly state: (dirOptions?: DirOptions | boolean) => string;
+	/** Returns a priority-sorted list of possible directories for configuration file storage (includes `paths.config()` as the first entry). */
 	readonly configDirs: (dirOptions?: DirOptions | boolean) => readonly string[];
+	/** Returns a priority-sorted list of possible directories for data file storage (includes `paths.data()` as the first entry). */
 	readonly dataDirs: (dirOptions?: DirOptions | boolean) => readonly string[];
+
+	/** Application name used for path construction (from supplied configuration or auto-generated). */
+	readonly $name: () => string;
+	/** Default isolation mode used by the particular `XDGAppPaths` instance. */
+	readonly $isolated: () => boolean;
 };
 
 function isBoolean<T>(t: T | boolean): t is boolean {
