87081259b4fe032cf18af08a985acf06fad940bd
Roy Ivy III <rivy.dev@gmail.com>
2021-02-21 23:00:59 -0600

    7.2.0

--- dist/cjs/mod.cjs.js
+++ dist/cjs/mod.cjs.js
@@ -1,6 +1,5 @@
 "use strict";
 var XDGAppPaths_js_1 = require("./lib/XDGAppPaths.js");
 var node_js_1 = require("./platform-adapters/node.js");
-var default_ = XDGAppPaths_js_1.Adapt(node_js_1.adapter).XDGAppPaths;
-module.exports = default_;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kLmNqcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9tb2QuY2pzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSx1REFBNkM7QUFFN0MsdURBQXNEO0FBRXRELElBQU0sUUFBUSxHQUFnQixzQkFBSyxDQUFDLGlCQUFPLENBQUMsQ0FBQyxXQUFXLENBQUM7QUFFekQsaUJBQVMsUUFBUSxDQUFDIn0=
+module.exports = XDGAppPaths_js_1.Adapt(node_js_1.adapter).XDGAppPaths;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kLmNqcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9tb2QuY2pzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSx1REFBNkM7QUFFN0MsdURBQXNEO0FBRXRELGlCQUFTLHNCQUFLLENBQUMsaUJBQU8sQ0FBQyxDQUFDLFdBQTBCLENBQUMifQ==
--- dist/types/mod.cjs.d.ts
+++ dist/types/mod.cjs.d.ts
@@ -1,25 +1,49 @@
+/** Configuration options supplied to `XDGAppPaths` methods; (optional) */
 declare type DirOptions = {
     readonly isolated?: boolean | null;
 };
+/** Configuration options supplied when constructing `XDGAppPaths`; (optional) */
 declare type Options = {
     readonly name?: string | null;
     readonly suffix?: string | null;
     readonly isolated?: boolean | null;
 };
-declare type XDGAppPaths = {
+/** `XDGAppPaths` (API) Determine (XDG-compatible) paths for storing application files (cache, config, data, etc) */
+interface XDGAppPaths {
+    /** Create an `XDG` object (`new` is optional). */
     new (options?: Options | string): XDGAppPaths;
+    /** Create an `XDG` object (`new` is optional). */
     (options?: Options | string): XDGAppPaths;
-    readonly $name: () => string;
-    readonly $isolated: () => boolean;
-    readonly cache: (dirOptions?: DirOptions | boolean) => string;
-    readonly config: (dirOptions?: DirOptions | boolean) => string;
-    readonly data: (dirOptions?: DirOptions | boolean) => string;
-    readonly runtime: (dirOptions?: DirOptions | boolean) => string | undefined;
-    readonly state: (dirOptions?: DirOptions | boolean) => string;
-    readonly configDirs: (dirOptions?: DirOptions | boolean) => readonly string[];
-    readonly dataDirs: (dirOptions?: DirOptions | boolean) => readonly string[];
-};
+    /** Returns the directory for non-essential data files.
+    > Deletion of the data contained here might cause an application to slow down.
+    */
+    cache(dirOptions?: DirOptions | boolean): string;
+    /** Returns the directory for config files.
+    > Deletion of the data contained here might require the user to reconfigure an application.
+    */
+    config(dirOptions?: DirOptions | boolean): string;
+    /** Returns the directory for data files.
+    > Deletion of the data contained here might force the user to restore from backups.
+    */
+    data(dirOptions?: DirOptions | boolean): string;
+    /** Returns the directory for runtime files; may return `undefined`.
+    > Deletion of the data contained here might interfere with a currently executing application but should have no effect on future executions.
+    */
+    runtime(dirOptions?: DirOptions | boolean): string | undefined;
+    /** Returns the directory for state files.
+    > Deletion of the data contained here should not materially interfere with execution of an application.
+    */
+    state(dirOptions?: DirOptions | boolean): string;
+    /** Returns a priority-sorted list of possible directories for configuration file storage (includes `paths.config()` as the first entry). */
+    configDirs(dirOptions?: DirOptions | boolean): readonly string[];
+    /** Returns a priority-sorted list of possible directories for data file storage (includes `paths.data()` as the first entry). */
+    dataDirs(dirOptions?: DirOptions | boolean): readonly string[];
+    /** Application name used for path construction (from supplied configuration or auto-generated). */
+    $name(): string;
+    /** Default isolation mode used by the particular `XDGAppPaths` instance. */
+    $isolated(): boolean;
+}
 
-declare const default_: XDGAppPaths;
+declare const _default: XDGAppPaths;
 
-export default default_;
+export = _default;
