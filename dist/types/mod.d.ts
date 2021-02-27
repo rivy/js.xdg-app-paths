80b315c23ebc988ce58da0b2c95b5f5b42e0f3b2
Roy Ivy III <rivy.dev@gmail.com>
2021-02-27 14:14:11 -0600

    7.3.0

--- dist/types/mod.d.ts
+++ dist/types/mod.d.ts
@@ -1,19 +1,25 @@
-/** Configuration options supplied to `XDGAppPaths` methods; (optional) */
-declare type DirOptions = {
+/** Configuration options supplied to `XDGAppPaths` methods */
+interface DirOptions {
+    /** Isolation flag; used to override the default isolation mode, when needed. */
     readonly isolated?: boolean | null;
-};
-/** Configuration options supplied when constructing `XDGAppPaths`; (optional) */
-declare type Options = {
+}
+/** Configuration options supplied when constructing `XDGAppPaths` */
+interface Options {
+    /** Name of the application; used to generate isolated application paths.
+    > When missing (`undefined`), `null`, or empty (`''`), it is generated automatically from the available process information.
+    */
     readonly name?: string | null;
+    /** Suffix which is appended to the application name when generating the application paths. */
     readonly suffix?: string | null;
+    /** Default isolation flag (used when no isolation flag is supplied for `DirOptions`). */
     readonly isolated?: boolean | null;
-};
-/** `XDGAppPaths` (API) Determine (XDG-compatible) paths for storing application files (cache, config, data, etc) */
+}
+/** `XDGAppPaths` (API) - Determine (XDG-compatible) paths for storing application files (cache, config, data, etc) */
 interface XDGAppPaths {
-    /** Create an `XDG` object (`new` is optional). */
-    new (options?: Options | string): XDGAppPaths;
-    /** Create an `XDG` object (`new` is optional). */
+    /** Create an `XDGAppPaths` object (a preceding `new` is optional). */
     (options?: Options | string): XDGAppPaths;
+    /** Create an `XDGAppPaths` object (`new` is optional). */
+    new (options?: Options | string): XDGAppPaths;
     /** Returns the directory for non-essential data files.
     > Deletion of the data contained here might cause an application to slow down.
     */
