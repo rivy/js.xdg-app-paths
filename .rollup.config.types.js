4177edd625efc45b173b690bddaf64fd35dea951
Roy Ivy III <rivy.dev@gmail.com>
2022-08-03 20:10:07 -0500

    maint/dev ~ revise `rollup` type bundling process
    - update `rollup` configuration, removing malfunctioning '@rollup/plugin-replace'
      - (will be) replaced with external `replace-in-file` tool

--- .rollup.config.types.js
+++ .rollup.config.types.js
@@ -1,8 +1,11 @@
+// `rollup` configuration
 // ref: <https://devhints.io/rollup>
+// v2002-07-14 [rivy]
 // setup: `npm i rollup @rollup/plugin-typescript` or `npm i rollup rollup-plugin-typescript2` (for visible TS error output)
 
+// spell-checker:ignore (people) Roy Ivy III * rivy
+
 import dts from 'rollup-plugin-dts';
-import replace from '@rollup/plugin-replace';
 
 export default [
 	// bundle TypeScript typings (TypeScript is unable/unwilling to do so...)
@@ -16,9 +19,7 @@ export default [
 	{
 		input: './build/types/src/mod.cjs.d.ts',
 		output: [{ file: './dist/types/mod.cjs.d.ts', format: 'cjs' }],
-		plugins: [
-			dts(),
-			replace({ 'export default ': 'export = ' }), // hack correct export for CJS default export
-		],
+		plugins: [dts()],
+		// * note: for correct interpretation of types by VSCode, './dist/types/mod.cjs.d.ts' requires a subsequent text replacement ("export { _default as default }" => "export = _default")
 	},
 ];
