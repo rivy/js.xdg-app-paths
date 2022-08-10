e59d677ffdd802184886e2809af6f1d4976efdd6
Roy Ivy III <rivy.dev@gmail.com>
2022-08-09 23:13:16 -0500

    update/deps ~ (up-to XDG-v10.5.0); uses std@0.134.0

--- src/platform-adapters/deno.deno.ts
+++ src/platform-adapters/deno.deno.ts
@@ -6,7 +6,7 @@
 import * as path from 'https://deno.land/std@0.150.0/path/mod.ts';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore // deno-type URL import
-import xdg from 'https://deno.land/x/xdg@v10.2.0/src/mod.deno.ts';
+import xdg from 'https://deno.land/x/xdg@v10.5.0/src/mod.deno.ts';
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
