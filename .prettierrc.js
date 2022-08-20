6bdb066b6b3286f61331d3801d353aec7bc23246
Roy Ivy III <rivy.dev@gmail.com>
2022-08-20 15:05:33 -0500

    maint/dev ~ (Prettier) update config; adds dual use (Deno or NodeJS projects)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,24 +1,18 @@
 // Prettier configuration
 // ref: <https://prettier.io/docs/en/options.html>
-// v2022-07-13 [rivy]
+// v2022-08-20 [rivy]
 
 // spell-checker:ignore (people) Roy Ivy III * rivy
+
+/* @prettier */ // note: (dprint) {.dprint.json}.prettier.associations should contain the name of this file
 
 module.exports = {
 	// $schema: 'http://json.schemastore.org/prettierrc',
 	printWidth: 100,
 	proseWrap: 'preserve',
 	singleQuote: true,
-	useTabs: true,
 	tabWidth: 2,
-	// "# overrides/[*.markdown]/tabWidth": "// set to 4 when https://github.com/prettier/prettier/issues/5019 is fixed",
-	overrides: [
-		{
-			files: ['*.md', '*.mkd', '*.markdown'],
-			options: {
-				tabWidth: 2,
-				useTabs: false,
-			},
-		},
-	],
+	useTabs: true,
+	// ## overrides/[*.markdown]/tabWidth": "// set this to 4 when/if https://github.com/prettier/prettier/issues/5019 is fixed",
+	overrides: [{ files: ['*.md', '*.mkd', '*.markdown'], options: { tabWidth: 2, useTabs: false } }],
 };
