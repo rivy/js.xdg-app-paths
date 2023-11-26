634fa6dfcd578ca66ee645613183f5d21c335406
Roy Ivy III <rivy.dev@gmail.com>
2023-11-26 16:06:10 -0600

    wip ~ update prettier config (semicolons = 'true', trailing commas = 'all')

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 // Prettier configuration
 // ref: <https://prettier.io/docs/en/options.html>
-// v2022-08-20 [rivy]
+// v2023-11-26 [rivy]
 
-// spell-checker:ignore (people) Roy Ivy III * rivy
+// spell-checker:ignore (names) DPrint (people) Roy Ivy III * rivy
 
 /* @prettier */ // note: (dprint) {.dprint.json}.prettier.associations should contain the name of this file
 
@@ -10,9 +10,11 @@ module.exports = {
 	// $schema: 'http://json.schemastore.org/prettierrc',
 	printWidth: 100,
 	proseWrap: 'preserve',
+	semi: true,
 	singleQuote: true,
 	tabWidth: 2,
+	trailingComma: 'all',
 	useTabs: true,
-	// ## overrides/[*.markdown]/tabWidth": "// set this to 4 when/if https://github.com/prettier/prettier/issues/5019 is fixed",
+	// ## overrides/[*.markdown]/tabWidth - may set this to 4 when/if https://github.com/prettier/prettier/issues/5019 is fixed
 	overrides: [{ files: ['*.md', '*.mkd', '*.markdown'], options: { tabWidth: 2, useTabs: false } }],
 };
