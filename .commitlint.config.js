23b33b4dd942b06d9045406be925b143d0de4950
Roy Ivy III <rivy.dev@gmail.com>
2026-04-19 23:32:10 -0500

    maint/dev ~ (CommitLint) update config (v2024.07.12; refactored)

--- .commitlint.config.js
+++ .commitlint.config.js
@@ -1,14 +1,15 @@
 // CommitLint configuration
 // ref: <https://commitlint.js.org/#/reference-configuration>
-// v2022-08-20 [rivy]
+// v2024-07-12 [rivy]
 
-// spell-checker:ignore (names) commitLint (people) Roy Ivy III * rivy (words) maint
+// spell-checker:ignore (jargon) maint (names) CommitLint DPrint (people) Roy Ivy III * rivy
 
-/* @prettier */ // note: (dprint) {.dprint.json}.prettier.associations should contain the name of this file
+/* @prettier */
+// note: (dprint) {.dprint.json}.prettier.associations should contain the name of this file
 
-const isNPMTestDist = !!process.env['npm_config_test_dist'];
-const isTestDist = !!process.env['test_dist'];
-const isTestRelease = !!process.env['test_release'];
+const isNPMTestDist = !!process.env.npm_config_test_dist;
+const isTestDist = !!process.env.test_dist;
+const isTestRelease = !!process.env.test_release;
 
 /** Relax linting rules/strictures (for development; *not* when submitting for distribution/release). */
 const relaxedReview = !(isNPMTestDist || isTestDist || isTestRelease);
@@ -47,6 +48,10 @@ const commitTags = [
 	...(relaxedReview ? ['VERSION', 'WIP', 'X'] : []),
 ];
 
+const RULE_DISABLE = 0;
+const RULE_WARNING = 1;
+const RULE_ERROR = 2;
+
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	parserPreset: {
@@ -76,15 +81,16 @@ module.exports = {
 	].filter((v) => v != null),
 	rules: {
 		// '@local/DEBUG': [1, 'always'],
-		'body-max-line-length': [0],
+		'body-max-line-length': [RULE_DISABLE],
 		// ## maint [2020-01-07; rivy] ~ 'footer-leading-blank' disabled until <https://github.com/conventional-changelog/commitlint/issues/896> is fixed
 		// ## ... refs: <https://github.com/conventional-changelog/commitlint/issues/896#issuecomment-671865868> , <https://github.com/rook/rook/pull/6499#issuecomment-717267089>
-		'footer-leading-blank': [0],
-		'header-max-length': [1, 'always', 90],
-		'scope-case': [2, 'always', ['camel-case', 'lower-case', 'pascal-case', 'upper-case']],
-		'subject-case': [0],
-		'subject-empty': [relaxedReview ? 1 : 2, 'never'],
-		'type-case': [2, 'always', ['lower-case', 'sentence-case']],
-		'type-enum': [2, 'always', [...commitTags.map((v) => v.toLowerCase()), ...commitTags]],
+		'footer-leading-blank': [RULE_DISABLE],
+		'footer-max-line-length': [RULE_DISABLE],
+		'header-max-length': [RULE_WARNING, 'always', 90],
+		'scope-case': [RULE_ERROR, 'always', ['camel-case', 'lower-case', 'pascal-case', 'upper-case']],
+		'subject-case': [RULE_DISABLE],
+		'subject-empty': [relaxedReview ? RULE_WARNING : RULE_ERROR, 'never'],
+		'type-case': [RULE_ERROR, 'always', ['lower-case', 'sentence-case']],
+		'type-enum': [RULE_ERROR, 'always', [...commitTags.map((v) => v.toLowerCase()), ...commitTags]],
 	},
 };
